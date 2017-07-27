a56d2de698087c8e04e720318e3c1ec9ea4317c4
Dennis Huillca <dennisbot@gmail.com>
2017-07-27 18:57:19 -0500

    py2: Puedes realizar una clase en python llamada "calculator_class" que tenga un método llamado "sum"

--- calculator.py
+++ calculator.py
@@ -1,16 +1,20 @@
 """
-Your module documentation here
+This Module is used to perform operations over collections
 """
 
 
 class CalculatorClass(object):
     """
-    Your class documentation here
+    This Class is used to perform aggregate functions
     """
 
     def sum(self, num_list):
         """
-        Your method documentation here
+        Just iterate through each index and add it into
+        an accumulator variable
         """
-        # your sum code here
-        return "not implement yet"  # Remove this dummy line
+        list_size = len(num_list)
+        acum = 0
+        for i in range(0, list_size):
+            acum += num_list[i]
+        return acum
